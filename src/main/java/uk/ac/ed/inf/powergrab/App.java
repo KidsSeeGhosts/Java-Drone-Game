a69935f640209895d56e391258f170cdd6be5a58
Ruairi O'Hare <ruairiohare@Ruairis-MacBook-Pro.local>
2019-09-28 13:56:04 +0100

    Made more efficient by doing 3 length calculations instead of 6 in nextPosition.Made lengths private static final double to also improve efficiency.

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -10,11 +10,17 @@ public class App
     {
         System.out.println( "Hello World!" );
         //Testing nextPosition
-        Position start = new Position(55.944425,-3.188396);
-        System.out.println(start.nextPosition(Direction.SSE).latitude);
-        System.out.println(start.nextPosition(Direction.SSE).longitude);
+        //Position start = new Position(55.944425,-3.188396);
+        ///System.out.println(start.nextPosition(Direction.SSE).latitude);
+        //System.out.println(start.nextPosition(Direction.SSE).longitude);
         //System.out.println((0.0003*Math.sin(67.5)));
         //System.out.println(Math.sin(67.5));
+        System.out.println(0.0003*Math.sin(Math.toRadians(67.5)));//h2
+        System.out.println(0.0003*Math.cos(Math.toRadians(67.5)));//w2
+        	System.out.println(0.0003*Math.sin(Math.toRadians(45))); //h3
+        	System.out.println(0.0003*Math.cos(Math.toRadians(45))); //w3
+        	System.out.println(0.0003*Math.sin(Math.toRadians(22.5))); //h4
+        	System.out.println(0.0003*Math.cos(Math.toRadians(22.5))); //w4
     }
     
 }
--- src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -7,14 +7,13 @@ public class Position {
     public double latitude;
     public double longitude;
     
-    //Trig height and width results are constants so all the distances don't need to be recalculated each time nextPosition is called in order to make the program more efficient.
-    //O.0003 is the hypotenuse length as it is the distance the drone moves in one move.
-	double h2 = 0.0003*Math.sin(Math.toRadians(67.5));//rsin67.5 height (67.5 degrees for going NNE,SSE,NNW,SSW)
-	double w2 = 0.0003*Math.cos(Math.toRadians(67.5));//rcos67.5 width
-	double h3 = 0.0003*Math.sin(Math.toRadians(45)); //rsin45 height (45 degrees for going NE,NW,SE,SW)
-	double w3 = 0.0003*Math.cos(Math.toRadians(45)); //rcos45 width
-	double h4 = 0.0003*Math.sin(Math.toRadians(22.5)); //rsin22.5 height (22.5 degrees for going ENE,ESE,WNW,WSW)
-	double w4 = 0.0003*Math.cos(Math.toRadians(22.5)); //rcos22.5 width
+    //My trig length results are static final constants, therefore the lengths are only calculated once and don't need to be recalculated...
+    //...each time nextPosition is called or a new position class is created in order to make the program more efficient.
+    //I am also able to calculate all next positions using only 3 lengths rather than 6 since cosine and sine equal opposite at certain degrees e.g height r(sin67.5) == width r(cos22.5).
+    //O.0003 is used as the hypotenuse length r as it is the distance the drone moves in one move.
+	private static final double h2 = 0.0003*Math.sin(Math.toRadians(67.5));//r*sin67.5 length
+	private static final double h3 = 0.0003*Math.sin(Math.toRadians(45)); //r*sin45 length
+	private static final double h4 = 0.0003*Math.sin(Math.toRadians(22.5)); //r*sin22.5 length
     
 	
     public Position (double latitude, double longitude) {//Defining position type for longitude,latitude coordinates.
@@ -31,50 +30,50 @@ public class Position {
     		if (direction == Direction.N) {
     			return new Position(latitude+0.0003,longitude);//0.0003 is how far the drone moves in one move.
     		}
-    		if (direction == Direction.NNE) {
-    			return new Position(latitude+h2,longitude+w2);
+    		if (direction == Direction.NNE) { //Adding to longitude and latitude as we go East.
+    			return new Position(latitude+h2,longitude+h4);
     		}
     		if (direction == Direction.NE) {
-    			return new Position(latitude+h3,longitude+w3);
+    			return new Position(latitude+h3,longitude+h3);
     		}
     		if (direction == Direction.ENE) {
-    			return new Position(latitude+h4,longitude+w4);
+    			return new Position(latitude+h4,longitude+h2);
     		}
     		if (direction == Direction.E) {
     			return new Position(latitude,longitude+0.0003);
     		}
-    		if (direction == Direction.ESE) {//Still adding to longitude, subtracting from latitude as we go south.
-    			return new Position(latitude-h4,longitude+w4);
+    		if (direction == Direction.ESE) {//Still adding to longitude, subtracting from latitude as we go South.
+    			return new Position(latitude-h4,longitude+h2);
     		}
     		if (direction == Direction.SE) {
-    			return new Position(latitude-h3,longitude+w3);
+    			return new Position(latitude-h3,longitude+h3);
     		}
     		if (direction == Direction.SSE) {
-    			return new Position(latitude-h2,longitude+w2);
+    			return new Position(latitude-h2,longitude+h4);
     		}
     		if (direction == Direction.S) {
     			return new Position(latitude-0.0003,longitude);
     		}
-    		if (direction == Direction.SSW) {//Now decreasing the longitude and latitude.
-    			return new Position(latitude-h2,longitude-w2);
+    		if (direction == Direction.SSW) {//Now decreasing the longitude and latitude as we go West.
+    			return new Position(latitude-h2,longitude-h4);
     		}
     		if (direction == Direction.SW) {
-    			return new Position(latitude-h3,longitude-w3);
+    			return new Position(latitude-h3,longitude-h3);
     		}
     		if (direction == Direction.WSW) {
-    			return new Position(latitude-h4,longitude-w4);
+    			return new Position(latitude-h4,longitude-h2);
     		}
     		if (direction == Direction.W) {
     			return new Position(latitude,longitude-0.0003);
     		}
-    		if (direction == Direction.WNW) {//Starting to increase latitude again and keep decreasing longitude.
-    			return new Position(latitude+h4,longitude-w4);
+    		if (direction == Direction.WNW) {//Starting to increase latitude again and continue decreasing longitude as we return to North.
+    			return new Position(latitude+h4,longitude-h2);
     		}
     		if (direction == Direction.NW) {
-    			return new Position(latitude+h3,longitude-w3);
+    			return new Position(latitude+h3,longitude-h3);
     		}
     		if (direction == Direction.NNW) {
-    			return new Position(latitude+h2,longitude-w2);
+    			return new Position(latitude+h2,longitude-h4);
     		}
     		else {//This else should never be reached as all valid direction types are covered.
     			return new Position(latitude,longitude);
