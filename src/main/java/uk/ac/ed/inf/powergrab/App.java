8fab62d89cf65279ccdcec8cc5da3b95d2062fe7
Ruairi O'Hare <ruairiohare@Ruairis-MacBook-Pro.local>
2019-09-28 14:01:07 +0100

    Removed print statements from App.java for submission reasons.

--- src/main/java/uk/ac/ed/inf/powergrab/App.java
+++ src/main/java/uk/ac/ed/inf/powergrab/App.java
@@ -15,12 +15,12 @@ public class App
         //System.out.println(start.nextPosition(Direction.SSE).longitude);
         //System.out.println((0.0003*Math.sin(67.5)));
         //System.out.println(Math.sin(67.5));
-        System.out.println(0.0003*Math.sin(Math.toRadians(67.5)));//h2
-        System.out.println(0.0003*Math.cos(Math.toRadians(67.5)));//w2
-        	System.out.println(0.0003*Math.sin(Math.toRadians(45))); //h3
-        	System.out.println(0.0003*Math.cos(Math.toRadians(45))); //w3
-        	System.out.println(0.0003*Math.sin(Math.toRadians(22.5))); //h4
-        	System.out.println(0.0003*Math.cos(Math.toRadians(22.5))); //w4
+        //System.out.println(0.0003*Math.sin(Math.toRadians(67.5)));//h2
+        //System.out.println(0.0003*Math.cos(Math.toRadians(67.5)));//w2
+        //	System.out.println(0.0003*Math.sin(Math.toRadians(45))); //h3
+        //	System.out.println(0.0003*Math.cos(Math.toRadians(45))); //w3
+        	//System.out.println(0.0003*Math.sin(Math.toRadians(22.5))); //h4
+        //	System.out.println(0.0003*Math.cos(Math.toRadians(22.5))); //w4
     }
     
 }
