d0c46290d12dad0db7658167e7d55dbe99ba5823
Ruairi O'Hare <s1724559@bartali.inf.ed.ac.uk>
2019-10-10 15:42:20 +0100

    Polished comments for cw1. Tested on DICE.

--- src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,7 +1,6 @@
 package uk.ac.ed.inf.powergrab;
 
 //This enum class only defines the list of constants for directions allowing us to call direction types like Direction.N etc.
-//This class has no other functionality.
 //We are using a 16 point compass with 22.5 degrees between each direction.
 public enum Direction {
 	N,
--- src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -7,83 +7,78 @@ public class Position {
     public double latitude;
     public double longitude;
     
-    //My trig length results are static final constants, therefore the lengths are only calculated once and don't need to be recalculated...
-    //...each time nextPosition is called or a new position class is created in order to make the program more efficient.
-    //I am also able to calculate all next positions using only 3 lengths rather than 6 since cosine and sine equal opposite at certain degrees e.g height r(sin67.5) == width r(cos22.5).
-    //O.0003 is used as the hypotenuse length r as it is the distance the drone moves in one move.
-	private static final double h2 = 0.0003*Math.sin(Math.toRadians(67.5));//r*sin67.5 length
-	private static final double h3 = 0.0003*Math.sin(Math.toRadians(45)); //r*sin45 length
-	private static final double h4 = 0.0003*Math.sin(Math.toRadians(22.5)); //r*sin22.5 length
+    //These are the 3 lengths needed to move the drone the correct distance in 22.5 degree increments, calculated only once.
+	private static final double length1 = 0.0003*Math.sin(Math.toRadians(67.5));
+	private static final double length2 = 0.0003*Math.sin(Math.toRadians(45));
+	private static final double length3 = 0.0003*Math.sin(Math.toRadians(22.5));
     
-	
-    public Position (double latitude, double longitude) {//Defining position type for longitude,latitude coordinates.
+	//Defining position type for longitude,latitude coordinates.
+    public Position (double latitude, double longitude) {
     		this.latitude = latitude;
     		this.longitude = longitude;
-    		
     }
 
     //Going North increases latitude.
     //Going South decreases latitude.
     //Going East increases longitude.
     //Going West decreases.
-    public Position nextPosition (Direction direction) {//This function returns the next position of the drone when it makes a move in the specified compass direction.
+    
+    //This function returns the next position of the drone after it makes a move in the specified compass direction.
+    public Position nextPosition (Direction direction) {
     		if (direction == Direction.N) {
     			return new Position(latitude+0.0003,longitude);//0.0003 is how far the drone moves in one move.
     		}
     		if (direction == Direction.NNE) { //Adding to longitude and latitude as we go East.
-    			return new Position(latitude+h2,longitude+h4);
+    			return new Position(latitude+length1,longitude+length3);
     		}
     		if (direction == Direction.NE) {
-    			return new Position(latitude+h3,longitude+h3);
+    			return new Position(latitude+length2,longitude+length2);
     		}
     		if (direction == Direction.ENE) {
-    			return new Position(latitude+h4,longitude+h2);
+    			return new Position(latitude+length3,longitude+length1);
     		}
     		if (direction == Direction.E) {
     			return new Position(latitude,longitude+0.0003);
     		}
     		if (direction == Direction.ESE) {//Still adding to longitude, subtracting from latitude as we go South.
-    			return new Position(latitude-h4,longitude+h2);
+    			return new Position(latitude-length3,longitude+length1);
     		}
     		if (direction == Direction.SE) {
-    			return new Position(latitude-h3,longitude+h3);
+    			return new Position(latitude-length2,longitude+length2);
     		}
     		if (direction == Direction.SSE) {
-    			return new Position(latitude-h2,longitude+h4);
+    			return new Position(latitude-length1,longitude+length3);
     		}
     		if (direction == Direction.S) {
     			return new Position(latitude-0.0003,longitude);
     		}
     		if (direction == Direction.SSW) {//Now decreasing the longitude and latitude as we go West.
-    			return new Position(latitude-h2,longitude-h4);
+    			return new Position(latitude-length1,longitude-length3);
     		}
     		if (direction == Direction.SW) {
-    			return new Position(latitude-h3,longitude-h3);
+    			return new Position(latitude-length2,longitude-length2);
     		}
     		if (direction == Direction.WSW) {
-    			return new Position(latitude-h4,longitude-h2);
+    			return new Position(latitude-length3,longitude-length1);
     		}
     		if (direction == Direction.W) {
     			return new Position(latitude,longitude-0.0003);
     		}
     		if (direction == Direction.WNW) {//Starting to increase latitude again and continue decreasing longitude as we return to North.
-    			return new Position(latitude+h4,longitude-h2);
+    			return new Position(latitude+length3,longitude-length1);
     		}
     		if (direction == Direction.NW) {
-    			return new Position(latitude+h3,longitude-h3);
+    			return new Position(latitude+length2,longitude-length2);
     		}
     		if (direction == Direction.NNW) {
-    			return new Position(latitude+h2,longitude-h4);
+    			return new Position(latitude+length1,longitude-length3);
     		}
     		else {//This else should never be reached as all valid direction types are covered.
     			return new Position(latitude,longitude);
     		}
     }
     
-    //The inPlayArea function returns true if the position is in the play area defined by 2 latitudes and 2 longitudes.
-    //The position latitude must be between 55.942617 and 55.946233.
-    //The position longitude must be between -3.192473 and -3.184319.
-    //The function returns false if the drone's position is not within the play area or on the edge of the play area.
+    //The inPlayArea function returns true if the position is inside the play area defined by 2 latitudes and 2 longitudes.
     public boolean inPlayArea() {
     		if (latitude<55.946233 && latitude>55.942617 && longitude<-3.184319 && longitude>-3.192473){
     			return true;
@@ -92,6 +87,4 @@ public class Position {
     			return false;
     		}
     }
-	
-	
 }
